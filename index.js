370478a90332c3a518722f23863680f5b7cbbbfb
Angelique Abacajan <angeliqueabacajan@Angeliques-MacBook-Air.local>
2021-09-14 14:47:26 -0400

    Added json packages || Completed task 1

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-fifaData.filter(function(item){
-    return item.Year === 2014;
-})
+const finals2014 = fifaData.filter(function(item) {
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+
+console.log(finals2014);
+
 //(a) Home Team name for 2014 world cup final
+console.log('task 1a', finals2014[0] ['Home Team Name']);
 
 //(b) Away Team name for 2014 world cup final
+console.log('task 1b', finals2014[0] ['Home Team Name']);
 
 //(c) Home Team goals for 2014 world cup final
+console.log('task 1c', finals2014[0] ['Away Team Name']);
 
 //(d) Away Team goals for 2014 world cup final
+console.log('task 1d', finals2014[0] ['Home Team Name']);
 
 //(e) Winner of 2014 world cup final */
-
+console.log('task 1e', finals2014[0] ['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -72,6 +79,7 @@ function getWinnersByYear(/* code here */) {
 }
 
 
+`   1+`
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher order function getAverageGoals to do the following: 
